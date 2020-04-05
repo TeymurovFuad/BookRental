fef9c77402971c001709fbbfb790f8d703ce6269
Fuad Teymurov <fuad.teymurov@github.com>
2020-04-06 01:51:13 +0400

    Adde Book and Genre classes

---
+++ Models/Genre.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BookRental.Models
+{
+    public class Genre
+    {
+        [Required]
+        public int genreIdPK { get; set; }
+        [Required]
+        public string name { get; set; }
+    }
+}
---
+++ Views/Home/Book.cs
@@ -0,0 +1,45 @@
+﻿using BookRental.Models;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookRental.Views.Home
+{
+    public class Book
+    {
+        [Required]
+        public int bookIdPK{ get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string tittle { get; set; }
+        [Required]
+        public string author { get; set; }
+        [Required]
+        public string description { get; set; }
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string imgUrl { get; set; }
+        [Required]
+        [Range(0,1000)]
+        public string availability { get; set; }
+        [Required]
+        [DataType(DataType.Currency)]
+        public double currency { get; set; }
+        [Required]
+        [DisplayFormat(DataFormatString ="{0: mm dd yyyy}")]
+        public DateTime? dataAdded { get; set; }
+        [Required]
+        public int genreId { get; set; }
+
+        public Genre Genre { get; set; }
+
+        [Required]
+        [DisplayFormat(DataFormatString = "{0: mm dd yyyy}")]
+        public DateTime publicationDate { get; set; }
+
+        [Required]
+        public int pages { get; set; }
+        [Required]
+        public string productDimensions { get; set; }
+    }
+}
