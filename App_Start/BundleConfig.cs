b9b168ad52c6541475d7193ec65e37ca09aa480b
Fuad Teymurov <fuad.teymurov@github.com>
2020-04-05 23:38:32 +0400

    Added admin dropdown

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -19,8 +19,9 @@ namespace BookRental
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+            bundles.Add(new ScriptBundle("~/bundles/bookrental").Include(
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/adminmenu.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
